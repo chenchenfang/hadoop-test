c1c15fc9d1054088b253e3dfd65916bedda1c5fc
fang7Home <857263385@qq.com>
2019-07-27 17:27:02 +0800

    修改了编译的bug

--- myhadooptest/src/main/java/fang7/wordcount/Driver.java
+++ myhadooptest/src/main/java/fang7/wordcount/Driver.java
@@ -5,6 +5,8 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 
+import org.apache.hadoop.io.compress.BZip2Codec;
+import org.apache.hadoop.io.compress.CompressionCodec;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -13,8 +15,14 @@ import java.io.IOException;
 
 public class Driver {
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
-        Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf);
+        Configuration configuration = new Configuration();
+        // 开启map端输出压缩
+        configuration.setBoolean("mapreduce.map.output.compress", true);
+        // 设置map端输出压缩方式
+        configuration.setClass("mapreduce.map.output.compress.codec", BZip2Codec.class, CompressionCodec.class);
+
+
+        Job job = Job.getInstance(configuration);
 
         job.setMapperClass(WordCountMapper.class);
         job.setReducerClass(WordCountReducer.class);
@@ -22,6 +30,13 @@ public class Driver {
 
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
+
+
+        // 设置reduce端输出压缩开启
+        FileOutputFormat.setCompressOutput(job, true);
+
+        // 设置压缩的方式
+        FileOutputFormat.setOutputCompressorClass(job, BZip2Codec.class);
 
         FileInputFormat.setInputPaths(job,new Path("D:\\hadoopInput\\wordcount"));
         FileOutputFormat.setOutputPath(job,new Path("D:\\hadoopOutput\\wordcount"));
