a648e0ddafc0bbdc8267428959ea76c3050e4073
方片7 <857263385@qq.com>
2019-08-06 09:52:01 +0800

    添加了多线程的测试

---
+++ myhadooptest/src/main/java/executorservice/ExeccuteTask.java
@@ -0,0 +1,116 @@
+package executorservice;
+
+public class ExeccuteTask {
+    public static void main(String[] args) {
+        ThreadPoolHelper.executeTask(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("222424");
+                try {
+                    Thread.sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        ThreadPoolHelper.executeTask(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("222424");
+                try {
+                    Thread.sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        ThreadPoolHelper.executeTask(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("222424");
+                try {
+                    Thread.sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        ThreadPoolHelper.executeTask(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("222424");
+                try {
+                    Thread.sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        ThreadPoolHelper.executeTask(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("222424");
+                try {
+                    Thread.sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });ThreadPoolHelper.executeTask(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("222424");
+                try {
+                    Thread.sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });ThreadPoolHelper.executeTask(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("222424");
+                try {
+                    Thread.sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });ThreadPoolHelper.executeTask(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("222424");
+                try {
+                    Thread.sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });ThreadPoolHelper.executeTask(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("222424");
+                try {
+                    Thread.sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });ThreadPoolHelper.executeTask(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("222424");
+                try {
+                    Thread.sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+
+
+
+    }
+}
