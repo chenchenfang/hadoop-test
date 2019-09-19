15c1d7cb71876e0dfa334e1ee427a8da637744fc
fang7Home <857263385@qq.com>
2019-09-19 23:27:39 +0800

    我来提交一次

---
+++ scala/src/main/scala/com/scala/basis/MyPrintTest.scala
@@ -0,0 +1,12 @@
+package com.scala.basis
+
+object MyPrintTest {
+  def main(args: Array[String]): Unit = {
+    var int1: Int=11
+    var int2: Int=22
+    var mystr1: String="143214"
+    printf("int1:%d,int2:%d,mystr1:%s \n",int1+1,int2,mystr1)
+    print(s"int1:$int1")
+  }
+
+}
