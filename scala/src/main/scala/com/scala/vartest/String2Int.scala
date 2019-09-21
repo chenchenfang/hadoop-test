2ab47c4d36969de2182c9155dc4adaeb8bc10923
fang7 <857263385@qq.com>
2019-09-21 22:41:05 +0800

    修改代码

--- scala/src/main/scala/com/scala/vartest/String2Int.scala
+++ scala/src/main/scala/com/scala/vartest/String2Int.scala
@@ -2,8 +2,11 @@ package com.scala.vartest
 
 object String2Int {
   def main(args: Array[String]): Unit = {
+
+    var str1: String ="12.5"
     //这个会报错
-    var str1: String ="12.5";
+    println(str1.toInt)
+    //这个不会报错
     println(math.round(str1.toDouble))
   }
 }
