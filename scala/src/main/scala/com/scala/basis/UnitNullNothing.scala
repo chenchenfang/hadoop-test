9b83ff32b854faf52a80ab8b9a06bdefafe00d39
fang7 <857263385@qq.com>
2019-09-21 17:36:24 +0800

    测试 一些null

--- scala/src/main/scala/com/scala/basis/UnitNullNothing.scala
+++ scala/src/main/scala/com/scala/basis/UnitNullNothing.scala
@@ -1,11 +1,29 @@
 package com.scala.basis
 
 object UnitNullNothing {
-  def main (args : Array[String]) : Unit ={
-    val unit = say()
-    println(unit)
-  }
-  def say(): Unit = {
+  def main(args: Array[String]): Unit = {
+    var test = new MyTest
+    test = null
+    println(test)
+
+    println(returnUnit())
+
+    returnNothing()
+
+
 
   }
+
+
+  def returnNothing():Nothing={
+    throw new Exception("我来抛异常了 ")
+  }
+  def returnUnit(): Unit = {
+
+  }
+}
+
+
+class MyTest {
+  var mm: Int = 1
 }
