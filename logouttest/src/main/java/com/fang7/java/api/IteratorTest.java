9ac38de1e079b52633043b07f8378efac5cfe8bb
fang7 <857263385@qq.com>
2019-09-27 18:25:22 +0800

    新测试的iterator

---
+++ logouttest/src/main/java/com/fang7/java/api/IteratorTest.java
@@ -0,0 +1,18 @@
+package com.fang7.java.api;
+
+import java.util.ArrayList;
+
+public class IteratorTest {
+    public static void main(String[] args) {
+        ArrayList<Integer> objects = new ArrayList<>();
+        objects.add(1);
+        objects.add(1);
+        objects.add(1);
+        objects.add(1);
+        objects.add(1);
+        objects.add(1);
+        objects.add(1);
+
+        objects.iterator().forEachRemaining(System.out::println);
+    }
+}
---
+++ scala/src/main/scala/com/scala/fortest/TwoDimensionalArray.scala
@@ -0,0 +1,20 @@
+package com.scala.fortest
+
+import Array._
+
+/**
+ * 二维数组置换
+ */
+object TwoDimensionalArray {
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+
+  def replacement(): Unit ={
+    val array = ofDim[Int](10,10)
+
+
+  }
+}
