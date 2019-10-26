883420007f476a71933bd4a8f04c48f38b18ff8b
fang7 <857263385@qq.com>
2019-10-26 21:27:14 +0800

    添加了定时删除过期的worker

--- scala/src/main/scala/com/scala/actor/sparkactors/common/RegisterWorkInfo.scala
+++ scala/src/main/scala/com/scala/actor/sparkactors/common/RegisterWorkInfo.scala
@@ -8,9 +8,13 @@ case class WorkInfo(id:String,cpu:Int,ram:Int){
   var lastHeartBeatTime:Long =System.currentTimeMillis()
 }
 
+//以下是spark用到的
 case class RegisteredWorkerInfo()
 
 case class SendHeartbeat()
 
 case class HeartBeat(id:String)
 
+case class StartTimeOutWorker()
+
+case class RemoveTimeOutWorker()
