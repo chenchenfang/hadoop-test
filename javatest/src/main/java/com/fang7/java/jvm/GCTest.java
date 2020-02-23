4b9a66e45cb87c2adf4de25eb0b49032043f6dc9
fang7 <xieziang123@gmail.com>
2020-02-23 20:55:54 +0800

    更新项目结构

--- javatest/src/main/java/com/fang7/java/jvm/GCTest.java
+++
@@ -1,9 +0,0 @@
-package com.fang7.java.jvm;
-
-public class GCTest {
-    public static void main(String[] args) {
-        byte[] allocation1, allocation2;
-        allocation1 = new byte[55000*1024];
-        allocation2 = new byte[9000 * 1024];
-    }
-}
